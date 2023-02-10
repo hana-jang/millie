0b222a61e38e7c776266a640dae11f20f2873ed8
hangjang <frontweb99@gmail.com>
2023-02-10 16:58:11 +0900

    230210
    학원에서 수정

--- js/index.js
+++ js/index.js
@@ -9,4 +9,16 @@ $(function(){
         $(".video-img-box img").hide();
         $(this).prev().attr("src","https://www.youtube.com/embed/u1OVbU4EdwI?autoplay=1&mute=1").show();
     });
+
+    $(".second-call-book-title li").click(function(){
+        var num=$(this).index();
+        // var num1=$(this).addClass("click");
+
+        $(this).addClass("click");
+        $(this).siblings().removeClass("click");
+        $(".book-click").eq(num).fadeIn();
+        $(".book-click").eq(num).siblings().fadeOut();
+
+    });
+    $(".second-call-book-title li:eq(0)").trigger("click");
 });
