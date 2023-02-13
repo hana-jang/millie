db19a1a4ac85daa172993c5cd49841b2dddacb14
hangjang <frontweb99@gmail.com>
2023-02-13 18:38:44 +0900

    230213
    스와이퍼 적용함

--- js/index.js
+++ js/index.js
@@ -12,13 +12,30 @@ $(function(){
 
     $(".second-call-book-title li").click(function(){
         var num=$(this).index();
-        // var num1=$(this).addClass("click");
 
-        $(this).addClass("click");
-        $(this).siblings().removeClass("click");
+        $(this).children().addClass("click");
+        $(this).siblings().children().removeClass("click");
         $(".book-click").eq(num).fadeIn();
         $(".book-click").eq(num).siblings().fadeOut();
 
     });
     $(".second-call-book-title li:eq(0)").trigger("click");
+
+
+    $(".audiobook-list ul li").click(function(){
+        var clicknum=$(this).index();
+
+        $(this).addClass("audiobook-list-click");
+        $(this).siblings().removeClass("audiobook-list-click");
+        $(".audiobook-main-wrap").eq(clicknum).show();
+        $(".audiobook-main-wrap").eq(clicknum).siblings().hide();
+
+    });
+
+
+    $(".review-wrap img").on("click",function(){
+        $(this).hide();
+        $(this).next().attr("src","https://www.youtube.com/embed/c-XG8k_w1LM?autoplay=1&mute=1").show();
+        $(".playbtn1").hide();
+    })
 });
